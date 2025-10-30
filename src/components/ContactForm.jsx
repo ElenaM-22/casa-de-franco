68fc77af57e77e70b2ed839f07db7c2d28c355c9
ElenaM-22 <122303552+ElenaM-22@users.noreply.github.com>
2025-10-30 12:37:09 -0400

    testing

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,44 @@
+import "./../css/index.css";
+import { useState } from 'react';
+
+export default function ContactForm() {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "2aa8a7c4-78b9-41ca-ae04-93643c34b79b");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="name" required/>
+        <input type="email" name="email" required/>
+        <textarea name="message" required></textarea>
+
+        <button type="submit">Submit Form</button>
+
+      </form>
+      <span>{result}</span>
+
+    </div>
+  );
+}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import "./../css/index.css";
 import "./../css/Contact.css";
 import fado1 from "./../images/gpt-fado.png";
+import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
     return (
@@ -14,34 +15,7 @@ const Contact = () => {
                                 <li>Telephone: +1(000)000-0000</li>
                                 <li>Catering Inquiries: some_email@casadefranco.com</li>
                                 <li>
-                                    <section className="one">
-                                        <h3>Let us know how we're doing!</h3>
-                                        <form method="POST" id="contact-form">
-
-                                            <input type="hidden" name="access_key" value="2aa8a7c4-78b9-41ca-ae04-93643c34b79b"/>
-
-                                            <p>
-                                                <label htmlFor="name">Your Name:</label>
-                                                <input id="name" type="text" name="name" required/>
-                                            </p>
-                                            <p>
-                                                <label htmlFor="email">Your Email:</label>
-                                                <input id="email" type="email" name="email" required/>
-                                            </p>
-                                            <p>
-                                                <label htmlFor="message">What would you like to tell us today? </label>
-                                                <textarea id="message" name="message" required></textarea>
-                                            </p>
-                                            <input type="checkbox" name="botcheck" className="hidden" style={{display: 'none'}} />
-
-                                            <p><button type="submit">Submit Form</button></p>
-
-                                            <div id="contact-result">
-                                            </div>
-                                            
-                                            <p id="message-sent"></p>
-                                        </form>
-                                    </section>
+                                    <ContactForm />
                                 </li>
                             </ol>
                         </div>
